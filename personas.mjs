6e8d9dd080e191e8ef643a19a4edc31c5c29d497
Sangho Kim (Sam) <rlaace423@gmail.com>
2025-06-22 01:52:10 +0900

    페르소나 추가

--- personas.mjs
+++ personas.mjs
@@ -1,6 +1,8 @@
 const personas = [
   {
     name: 'Quintin',
+    age: 40,
+    sex: 'Male',
     persona:
       'Quintin, a 40-year-old logistician from Converse, TX, balances curiosity with practicality, appreciating both new ideas and established methods, and maintains a unique blend of organization and flexibility in all aspects of life.',
     arts_persona:
@@ -8,6 +10,8 @@ const personas = [
   },
   {
     name: 'Mauricio',
+    age: 20,
+    sex: 'Male',
     persona:
       'Mauricio, a reserved yet curious 20-year-old nurse with a passion for photography, balances ambition with pragmatism, preferring intimate gatherings over large social events.',
     arts_persona:
@@ -15,10 +19,30 @@ const personas = [
   },
   {
     name: 'Bonnie',
+    age: 23,
+    sex: 'Female',
     persona:
       'Bonnie, a sociable Midwesterner, balances curiosity with practicality, hosting potlucks one moment and creating intricate spreadsheets the next, always with a competitive spark in her eye.',
     arts_persona:
       'Bonnie, appreciating both new ideas and established methods, enjoys local folk music performances and occasionally visits art galleries, finding inspiration in the works of Grandma Moses.',
+  },
+  {
+    name: 'Abby',
+    age: 19,
+    sex: 'Female',
+    persona:
+      'Abby, a curious and adaptable 19-year-old, is a jack-of-all-trades who balances their love for new experiences with a practical, down-to-earth approach to life, often leaving a trail of half-read books and unfinished art projects in their wake.',
+    arts_persona:
+      'They spend their free time exploring local art galleries, having recently developed an appreciation for the vivid colors and storytelling of Romare Bearden, while still holding a soft spot for the classical melodies of Beethoven.',
+  },
+  {
+    name: 'Sonia',
+    age: 70,
+    sex: 'Female',
+    persona:
+      'Sonia, a 70-year-old retiree, is a vibrant, imaginative hostess who combines her love for Mexican culture and art with a touch of healthy competition.',
+    arts_persona:
+      "Sonia, a passionate artist, finds inspiration in Frida Kahlo's bold self-portraits and Octavio Paz's poetic explorations of Mexican identity, incorporating these influences into her own work.",
   },
 ];
 
